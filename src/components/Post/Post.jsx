eb153fe4cf436881df5a3e86378fa15cee5ab240
talles <talles-guerra@hotmail.com>
2025-03-26 14:37:56 +0000

    key's updated in the Post and App.jsx

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -63,11 +63,13 @@ export function Post({ author, publishedAt, content }) {
       <div className={styles.content}>
         {content.map((line) => {
           if (line.type === "paragraph") {
-            return <p>{line.content}</p>;
+            return <p key={line.content}>{line.content}</p>;
           } else if (line.type === "link") {
-            return <a href={line.content}>{line.content}</a>;
-          } else {
-            return null;
+            return (
+              <p key={line.content}>
+                <a href={line.content}>{line.content}</a>
+              </p>
+            );
           }
         })}
       </div>
@@ -92,7 +94,7 @@ export function Post({ author, publishedAt, content }) {
         ))} */}
 
         {comments.map((comment) => {
-          return <Comment content={comment} />;
+          return <Comment key={comment} content={comment} />;
         })}
       </div>
     </article>
